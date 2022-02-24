f36b4620a58b65710d0bc73285c3ab8294393871
GaiaPizzaria <100325405+GaiaPizzaria@users.noreply.github.com>
2022-02-23 22:07:38 -0300

    Add files via upload

---
+++ conversa.py
@@ -0,0 +1,18 @@
+#print('Olá! Seja bem vindo ao programa do Marco!')
+import time
+#time.sleep (2)
+#print ('Gostaria de fazer algumas perguntas, vamos começar?')
+#time.sleep (2)
+#nome = input ("qual o seu nome?")
+#time.sleep (2)
+#print("Bem vindo",nome,'!')
+#time.sleep(2)
+idade = input ('Quantos anos você tem?')
+print ("Você tem",idade, "anos certo?")
+respostaidade = input ()
+if respostaidade == ('nao') : idade = input ('Vamos tentar de novo? Quantos anos você tem?')
+print ("Você tem",idade, "anos certo?")
+if respostaidade == ('nao') : idade = input ('Vamos tentar de novo? Quantos anos você tem?')
+if respostaidade == ('sim') : (print ('maravilha'))
+print ('Muito obrigado por conversar comigo!')
+
